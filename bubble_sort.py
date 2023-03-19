0772f3e5bc028e81d4a4564c5939da7f080ef473
Andrew Maged <82352544+AndrewMaged814@users.noreply.github.com>
2023-03-19 02:49:02 +0200

    refined the whole code

--- bubble_sort.py
+++ bubble_sort.py
@@ -4,88 +4,82 @@ from OpenGL.GLU import *
 import random
 import time
 
-# global variables
-data = []
-N = 20  # size of data
-i = j = 0
-swapping = False
-reset = False
-bar_width = 0.8
+class BubbleSortVisualizer:
+    def __init__(self, N):
+        self.N = N
+        self.data = []
+        self.i = 0
+        self.j = 0
+        self.swapping = False
+        self.sorting_completed = False
+        self.reset = False
+        self.bar_width = 0.8
 
+    def init(self):
+        glClearColor(0.1, 0.1, 0.1, 1.0)
+        glColor3f(1.0, 1.0, 1.0)
+        gluOrtho2D(0, self.N, 0, 1)
+        self.generate_data()
 
-def init():
-    glClearColor(0.1, 0.1, 0.1, 1.0)
-    glColor3f(1.0, 1.0, 1.0)
-    gluOrtho2D(0, N, 0, 1)
-    generate_data()
+    def display(self):
+        glClear(GL_COLOR_BUFFER_BIT)
+        if self.reset:
+            self.generate_data()
+            self.reset = False
+        self.draw_data()
+        glutSwapBuffers()
 
+    def draw_data(self):
+        for k in range(self.N):
+            x = k + 0.1
+            y = self.data[k]
+            if self.sorting_completed or k >= self.N - self.i:
+                glColor3f(0.0, 1.0, 0.0)
+            else:
+                glColor3f(1.0, 1.0, 1.0)
+            glRectf(x, 0, x + self.bar_width, y)
 
-def display():
-    global reset
-    glClear(GL_COLOR_BUFFER_BIT)
-    if reset:
-        generate_data()
-        reset = False
-    draw_data()
-    glutSwapBuffers()
+    def generate_data(self):
+        self.data = [random.uniform(0, 1) for _ in range(self.N)]
 
-
-def draw_data():
-    for k in range(N):
-        x = k + 0.1
-        y = data[k]
-        if swapping and (k == j or k == j+1):
-            glColor3f(1.0, 0.0, 0.0)  # set color to red if being compared
-        elif k > N - i - 1:
-            glColor3f(0.0, 1.0, 0.0)  # set color to green if in final position
-        else:
-            glColor3f(1.0, 1.0, 1.0)  # set color to white otherwise
-        glRectf(x, 0, x + bar_width, y)
-
-
-def generate_data():
-    global data
-    data = [random.uniform(0, 1) for _ in range(N)]
-
-
-def bubble_sort():
-    global i, j, swapping
-    if i < N - 1:
-        if j < N - i - 1:
-            if data[j] > data[j + 1]:
-                data[j], data[j + 1] = data[j + 1], data[j]
-                swapping = True
-            j += 1
-            glutPostRedisplay()
+    def bubble_sort(self):
+        if self.i < self.N - 1:
+            if self.j < self.N - self.i - 1:
+                if self.data[self.j] > self.data[self.j + 1]:
+                    self.data[self.j], self.data[self.j + 1] = self.data[self.j + 1], self.data[self.j]
+                    self.swapping = True
+                self.j += 1
+            else:
+                self.i += 1
+                self.j = 0
             time.sleep(0.05)
         else:
-            i += 1
-            j = 0
-    else:
-        swapping = False
-        glutSetWindowTitle("Bubble Sort Complete")
-
-
-def keyboard(key, x, y):
-    global reset, swapping, i, j
-    if key == b'a' and not swapping:
-        i = j = 0
-        glutIdleFunc(bubble_sort)
-    elif key == b'r':
-        reset = True
-        swapping = False
+            self.swapping = False
+            self.sorting_completed = True
+            glutSetWindowTitle("Bubble Sort Complete")
+            glutIdleFunc(None)  
+            self.draw_data() 
         glutPostRedisplay()
 
+    def keyboard(self, key, x, y):
+        if key == b'a' and not self.swapping:
+            self.i = self.j = 0
+            glutIdleFunc(self.bubble_sort)
+        elif key == b'r':
+            self.reset = True
+            self.swapping = False
+            glutPostRedisplay()
 
 def main():
+    N = 20
+    visualizer = BubbleSortVisualizer(N)
     glutInit()
     glutInitWindowSize(700, 700)
     glutCreateWindow("Bubble Sort")
-    init()
-    glutDisplayFunc(display)
-    glutKeyboardFunc(keyboard)
+    visualizer.init()
+    glutDisplayFunc(visualizer.display)
+    glutKeyboardFunc(visualizer.keyboard)
     glutMainLoop()
-
 
 if __name__ == '__main__':
     main()
